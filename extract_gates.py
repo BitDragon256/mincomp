38de078ac098dc1f11489a5d298b4131b6625427
Benedict Sondershaus <bergben44@gmail.com>
2024-01-27 11:34:43 +0100

    save gate blocks

--- extract_gates.py
+++ extract_gates.py
@@ -3,8 +3,10 @@ from amulet.api.block import Block
 from amulet.utils.world_utils import block_coords_to_chunk_coords
 from amulet_nbt import StringTag, IntTag
 
+import numpy as np
+
 import world
-from gate import MinecraftGateBlueprint
+from gate import MinecraftGateBlueprint, BlockArray
 
 chunks_to_check = [(-1, 1)]
 overworld = "minecraft:overworld"
@@ -94,19 +96,25 @@ def get_gates(level, boundings):
         inports, outports = [], []
 
         start, end = bounding
+        blocks = BlockArray(
+            end[0] - start[0] + 1,
+            end[1] - start[1] + 1,
+            end[2] - start[2] + 1
+        )
         for x in range(start[0], end[0] + 1):
             for z in range(start[2], end[2] + 1):
                 (cx, cz), (ox, oz) = to_chunk(level, x, z)
                 chunk = level.get_chunk(cx, cz, overworld)
                 for y in range(start[1], end[1] + 1):
                     block = chunk.block_palette[chunk.blocks[ox, y, oz]]
+                    blocks[x - start[0], y - start[1], z - start[2]] = block
                     loc = (x, y, z)
                     if block == universal_inport_block:
                         inports.append(loc)
                     elif block == universal_outport_block:
                         outports.append(loc)
 
-        (cx, cy, cz) = bounding[0] # center
+        (cx, cy, cz) = bounding[0]  # center
         for inport in inports:
             x, y, z = inport
             inport = (x - cx, y - cy, z - cz)
@@ -117,11 +125,9 @@ def get_gates(level, boundings):
         gates.append(MinecraftGateBlueprint(
             inports,
             outports,
-            bounding
+            bounding,
+            blocks
         ))
-        print("inports: ", inports)
-        print("outports: ", outports)
-        print("bounding: ", bounding)
     return gates
 
 
--- gate.py
+++ gate.py
@@ -1,7 +1,51 @@
+import amulet
+from amulet.api.block import Block
+
+
+class BlockArray:
+    def __init__(self, sx, sy, sz):
+        self.size = (sx, sy, sz)
+        self.data = [[[Block("universal_minecraft", "air") for z in range(sz)] for y in range(sy)] for x in range(sx)]
+
+    def __getitem__(self, key):
+        x, y, z = key
+        self.check_bounds(key)
+        return self.data[x][y][z]
+
+    def __setitem__(self, key, value):
+        x, y, z = key
+        self.check_bounds(key)
+        self.data[x][y][z] = value
+
+    def __repr__(self):
+        s = ""
+        sx, sy, sz = self.size
+        for y in range(sy):
+            s += "["
+            for z in range(sz):
+                s += "["
+                for x in range(sx):
+                    s += f"{self[x, y, z]}"
+                    if x != sx - 1:
+                        s += ", "
+                s += "]"
+                if z != sz - 1:
+                    s += ",\n"
+            s += "]"
+            if y != sy - 1:
+                s += ",\n\n"
+
+        return s
+
+    def check_bounds(self, key):
+        for i in range(3):
+            if key[i] < 0 or key[i] >= self.size[i]:
+                raise Exception(f"index {key} is out of bounds for array of size {self.size}")
+
 
 class MinecraftGateBlueprint:
-    def __init__(self, inport_positions, outport_positions, bounding_box):
+    def __init__(self, inport_positions, outport_positions, bounding_box, blocks):
         self.inport_positions = inport_positions
         self.outport_positions = outport_positions
         self.bounding_box = bounding_box
-
+        self.blocks = blocks
